59295f9d13d532d300f0eaf2d16ad14539ae4477
faith-ye <m202171620@hust.edu.cn>
2023-11-05 10:36:44 +0800

    faith-ye，feat：test

--- chatbot-api-interfaces/src/test/java/cn/hust/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/hust/chatbot/api/test/ApiTest.java
@@ -1,5 +1,20 @@
 package cn.hust.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 /**
  * @className: ApiTest
  * @description: 单元测试
@@ -8,4 +23,80 @@ package cn.hust.chatbot.api.test;
  * @version: 1.0
  */
 public class ApiTest {
+
+    @Test
+    public void base64(){
+        String cronExpression = new String(Base64.getDecoder().decode("MC81MCAqICogKiAqID8="), StandardCharsets.UTF_8);
+        System.out.println(cronExpression);
+    }
+
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/48884181422288/topics?scope=unanswered_questions&count=20");
+
+        get.addHeader("cookie", "zsxq_access_token=665B0C36-A4B0-A275-0986-3C13E513A683_C81538A43D9850CF; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22185455181181852%22%2C%22first_id%22%3A%22187461d7431f7f-02b73d085b9586c-26031851-1327104-187461d74321e8%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg3NDYxZDc0MzFmN2YtMDJiNzNkMDg1Yjk1ODZjLTI2MDMxODUxLTEzMjcxMDQtMTg3NDYxZDc0MzIxZTgiLCIkaWRlbnRpdHlfbG9naW5faWQiOiIxODU0NTUxODExODE4NTIifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22185455181181852%22%7D%2C%22%24device_id%22%3A%22187461d7431f7f-02b73d085b9586c-26031851-1327104-187461d74321e8%22%7D; zsxqsessionid=cabf9c521333dd57e3f35e5628c11605; abtest_env=product");
+        get.addHeader("Content-Type", "application/json;charset=utf8");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/48884181422288/answer");
+        post.addHeader("cookie", "zsxq_access_token=665B0C36-A4B0-A275-0986-3C13E513A683_C81538A43D9850CF; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22185455181181852%22%2C%22first_id%22%3A%22187461d7431f7f-02b73d085b9586c-26031851-1327104-187461d74321e8%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg3NDYxZDc0MzFmN2YtMDJiNzNkMDg1Yjk1ODZjLTI2MDMxODUxLTEzMjcxMDQtMTg3NDYxZDc0MzIxZTgiLCIkaWRlbnRpdHlfbG9naW5faWQiOiIxODU0NTUxODExODE4NTIifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22185455181181852%22%7D%2C%22%24device_id%22%3A%22187461d7431f7f-02b73d085b9586c-26031851-1327104-187461d74321e8%22%7D; zsxqsessionid=cabf9c521333dd57e3f35e5628c11605; abtest_env=product");
+        post.addHeader("Content-Type", "application/json;charset=utf8");
+
+        String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"自己去百度！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": false\n" +
+                "  }\n" +
+                "}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void test_chatGPT() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.openai.com/v1/completions");
+        post.addHeader("Content-Type", "application/json");
+        post.addHeader("Authorization", "Bearer 自行申请 https://beta.openai.com/overview");
+
+        String paramJson = "{\"model\": \"text-davinci-003\", \"prompt\": \"帮我写一个java冒泡排序\", \"temperature\": 0, \"max_tokens\": 1024}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+
+    }
+
 }
